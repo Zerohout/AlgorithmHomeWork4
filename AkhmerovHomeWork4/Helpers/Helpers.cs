d52294bb5877689902088b4702318320f5c94853
Zerohout <zerohout@gmail.com>
2018-07-21 22:06:59 +0300

    Добавьте файлы проекта.

---
+++ AkhmerovHomeWork4/Helpers/Helpers.cs
@@ -0,0 +1,118 @@
+﻿namespace AkhmerovHomeWork4.Helpers
+{
+    using System;
+    using static System.Console;
+
+    class Helpers
+    {
+
+        /// <summary>
+        /// Прорисовка шахматного поля на основе полученного двумерного массива
+        /// </summary>
+        /// <param name="chessField">Двумерный массив (шахматное поле)</param>
+
+        public static void DrawChessField(char[,] chessField)
+        {
+            var height = chessField.GetLength(0);
+            var width = chessField.GetLength(1);
+            Clear();
+            WriteLine();
+            for (var i = 0; i < chessField.GetLength(0); i++)
+            {
+                CursorTop = (WindowHeight / 2 - height) + i * 2;
+                CursorLeft = WindowWidth / 2 - (int)(width * 1.5);
+
+                for (var j = 0; j < chessField.GetLength(1); j++)
+                {
+                    Write($" {chessField[i, j]} ");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Создание шахматного поля на основе полученного двумерного массива
+        /// </summary>
+        /// <param name="chessField">Двумерный массив с размером шахматного поля</param>
+        /// <param name="emptyCell">Символ пустой клетки</param>
+        /// <returns>Двумерный массив (шахматное поле)</returns>
+        public static void CreateChessField(ref char[,] chessField, char emptyCell)
+        {
+            for (var i = 0; i < chessField.GetLength(0); i++)
+            {
+                for (var j = 0; j < chessField.GetLength(1); j++)
+                {
+                    chessField[i, j] = emptyCell;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Невозможность решить заданную задачу.
+        /// </summary>
+
+        public static void ImpossibleTask()
+        {
+            Clear();
+            const string impossible = "Задачу невозможно решить.";
+            CursorTop = WindowHeight / 2;
+            CursorLeft = WindowWidth / 2 - impossible.Length;
+            ForegroundColor = ConsoleColor.Red;
+            Write(impossible);
+            ResetColor();
+        }
+
+        /// <summary>
+        /// Проверка успешности решения задачи
+        /// </summary>
+        /// <param name="field">Двумерный массив (Шахматное поле)</param>
+        /// <param name="finishTurns">Массив с успешными ходами</param>
+        /// <param name="stats">Структура статистики</param>
+        /// <param name="cells">Структура обозначений</param>
+
+        public static void CheckFinish(char[,] field, string[] finishTurns, Statistics stats, Cells cells)
+        {
+            var height = field.GetLength(0);
+            var width = field.GetLength(1);
+
+            for (var i = 0; i < height; i++)
+            {
+                for (var j = 0; j < width; j++)
+                {
+                    if (field[i, j] == cells.emptyCell)
+                    {
+                        return;
+                    }
+                }
+            }
+
+            TaskFinish(stats, finishTurns);
+        }
+
+        /// <summary>
+        /// Задача выполнена
+        /// </summary>
+        /// <param name="stats">Структура статистики</param>
+        /// <param name="finishTurns">Массив успешных ходов</param>
+
+        private static void TaskFinish(Statistics stats, string[] finishTurns)
+        {
+            stats.allOperations += stats.clearOperations;
+
+            ForegroundColor = ConsoleColor.Green;
+            const string finish = "Задача успешно завершена!";
+            WriteLine("\n\n");
+            CursorLeft = WindowWidth / 2 - finish.Length;
+            Write(finish);
+            ResetColor();
+
+            WriteLine("\n Выполненные ходы: \n");
+
+            foreach (var turn in finishTurns)
+            {
+                WriteLine(turn);
+            }
+
+            Write($"\nЧистых операций (без учета количества отмен ходов): {stats.clearOperations:N0}\nВсех операций: {stats.allOperations:N0}");
+        }
+    }
+}
---
+++ AkhmerovHomeWork4/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace AkhmerovHomeWork4
+{
+    using Algorithms;
+    using static System.Console;
+    using static Helpers.Helpers;
+
+    #region Структуры
+
+    /// <summary>
+    /// Структура обозначений клеток на шахматном поле
+    /// </summary>
+
+    struct Cells
+    {
+        /// <summary>
+        /// Обозначение пустой клетки
+        /// </summary>
+        public char emptyCell;
+        /// <summary>
+        /// Обозначение клетки с фигурой
+        /// </summary>
+        public char horseCell;
+        /// <summary>
+        /// Обозначение использованных фигурой клеток
+        /// </summary>
+        public char usedTurnCell;
+    }
+
+    /// <summary>
+    /// Структура позиций фигуры "Конь"
+    /// </summary>
+
+    struct HorsePosition
+    {
+        /// <summary>
+        /// Позиция фигуры по вертикали
+        /// </summary>
+        public int posY;
+        /// <summary>
+        /// Позиция фигуры по горизонтали
+        /// </summary>
+        public int posX;
+    }
+
+    /// <summary>
+    /// Структура переменных статистики
+    /// </summary>
+
+    struct Statistics
+    {
+        /// <summary>
+        /// Статистика чистых операций (без подсчета отмены ходов)
+        /// </summary>
+        public long clearOperations;
+        /// <summary>
+        /// Статистика всех операций (включая отмены хода)
+        /// </summary>
+        public long allOperations;
+    }
+
+    /// <summary>
+    /// Структура технических переменных
+    /// </summary>
+
+    struct TechnicalVariables
+    {
+        /// <summary>
+        /// Значение паузы после отрисовки шахматного поля
+        /// </summary>
+        public int pauseValue;
+        /// <summary>
+        /// Значение высоты поля
+        /// </summary>
+        public int fieldY;
+        /// <summary>
+        /// Значение ширины поля
+        /// </summary>
+        public int fieldX;
+    }
+
+    #endregion
+
+    class Program
+    {
+        static void Main()
+        {
+            var cells = new Cells
+            {
+                emptyCell = '.',
+                horseCell = 'O',
+                usedTurnCell = 'X'
+            };
+
+            var techVar = new TechnicalVariables
+            {
+                pauseValue = 0,
+                fieldY = 5,
+                fieldX = 5
+            };
+
+            var testChessField = new char[techVar.fieldY, techVar.fieldX];
+
+            CreateChessField(ref testChessField, cells.emptyCell);
+            DrawChessField(testChessField);
+
+            var algorithm = new SlowAlgorithm(testChessField, cells, techVar);
+            algorithm.StartSearching();
+
+            ReadKey();
+        }
+    }
+}
